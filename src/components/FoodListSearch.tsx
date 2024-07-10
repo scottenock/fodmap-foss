0467641d702a80d8bda37da8afe98f0bf5536319
ScottEnock <scott@enock.ovh>
2024-07-10 19:31:51 +0100

    feat: adjust search styling

--- src/components/FoodListSearch.tsx
+++ src/components/FoodListSearch.tsx
@@ -24,11 +24,11 @@ const FoodListSearch: React.FC<FoodListSearchProps> = ({ setFoods, foods }) => {
     <div className="bg-green-400 p-2 flex items-center">
       <input
         placeholder="search..."
-        className="appearance-none border-2 placeholder:text-white bg-green-400 border-green-300 rounded-full w-full mr-3 py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:text-black text-white focus:border-white"
+        className="appearance-none border-2 placeholder:text-white bg-green-400 border-green-300 rounded-full w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:text-black text-white focus:border-white"
         type="text"
         onChange={handleChange}
       />
-      <img src={options} className="w-5 h-5" />
+      <img src={options} className="w-5 h-5 mx-3" />
     </div>
   );
 };
