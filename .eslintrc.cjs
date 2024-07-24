de17a21e1ac188de679933a0885d836926821999
ScottEnock <scott@enock.ovh>
2024-07-24 17:43:02 +0100

    feat: optimise FoodList rendering (infinate-scroller)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,17 +10,6 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["react-refresh", "unused-imports"],
   rules: {
-    "typescript-eslint/no-unused-vars": "off",
-    "unused-imports/no-unused-imports": "error",
-    "unused-imports/no-unused-vars": [
-      "warn",
-      {
-        vars: "all",
-        varsIgnorePattern: "^_",
-        args: "after-used",
-        argsIgnorePattern: "^_",
-      },
-    ],
     "react-refresh/only-export-components": [
       "warn",
       { allowConstantExport: true },
--- src/components/FoodList.tsx
+++ src/components/FoodList.tsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { Fodmap } from "../types/Fodmap";
 import Score from "./Score";
+import InfiniteScroll from "react-infinite-scroller";
+import { useState } from "react";
 
 type FoodListProps = {
   foods: Fodmap[];
@@ -10,29 +12,42 @@ const styles = {
   container: "flex justify-between items-center my-1 px-3",
 };
 
+const incrementValue = 25;
+
 const FoodList: React.FC<FoodListProps> = ({ foods }) => {
+  const [loadedItems, setLoadedItems] = useState(incrementValue);
+  const currentItems = foods.slice(0, loadedItems);
+
   return (
     <div>
-      {foods.map((food) => (
-        <div key={food.id}>
-          <Link to={`/food/${food.id}`}>
-            <div className={styles.container}>
-              <div>
-                <p className="text-lg">{food.name}</p>
-                <p className="text-base">{food.category}</p>
+      <InfiniteScroll
+        start={0}
+        hasMore={foods.length > currentItems.length}
+        loadMore={() => {
+          setLoadedItems((items) => items + incrementValue);
+        }}
+      >
+        {currentItems.map((food) => (
+          <div key={food.id}>
+            <Link to={`/food/${food.id}`}>
+              <div className={styles.container}>
+                <div>
+                  <p className="text-lg">{food.name}</p>
+                  <p className="text-base">{food.category}</p>
+                </div>
+                <div className="flex items-center my-1">
+                  <Score
+                    text={food.fodmap}
+                    score={food.fodmap === "high" ? 2 : 0}
+                    reversed={true}
+                  />
+                </div>
               </div>
-              <div className="flex items-center my-1">
-                <Score
-                  text={food.fodmap}
-                  score={food.fodmap === "high" ? 2 : 0}
-                  reversed={true}
-                />
-              </div>
-            </div>
-          </Link>
-          <div className="border-b-2 border-gray-300 mb-2" />
-        </div>
-      ))}
+            </Link>
+            <div className="border-b-2 border-gray-300 mb-2" />
+          </div>
+        ))}
+      </InfiniteScroll>
     </div>
   );
 };
